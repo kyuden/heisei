19ae3a1f9a7f8408a80c743b428191660279edb5
久田真寛 <kyuden@hisadamakotokan-no-MacBook-Pro.local>
2014-10-11 21:20:07 +0900

    Add era list

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,16 @@
 class MainController < ApplicationController
-  def index; end
+  def index
+    @eras = eras
+  end
+
+  private
+    def eras
+      (1989..Time.now.year).map do |year|
+         if year == 1989
+          {christian_year: year, year: "01"}
+         else
+          {christian_year: year, year: Date.new(year).to_era("%E")}
+        end
+      end.reverse
+    end
 end
