7793fe84769ce9a9dafd7d7da9415126129a3bc1
Kyuden <msmsms.um@gmail.com>
2014-10-12 00:17:18 +0900

    Add google analytics

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module Heisei
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.middleware.use Rack::GoogleAnalytics, :tracker => ENV["ANALYTICS_ID"]
   end
 end
